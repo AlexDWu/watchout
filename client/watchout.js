d30ba148cee8512092a8de972d438aebaf37e959
AlexDWu <hehahohee@gmail.com>
2015-12-04 15:41:58 -0800

    Added and image with D3, and repositioned it randomly.

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,19 @@
-// start slingin' some d3 here.
+
+var width = 600, height = 600;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("image")
+  .attr('xlink:href', "asteroid.png")
+  .attr("width", 60)
+  .attr("height", 60);
+
+setInterval(function() {
+  svg.select("image")
+  .style({
+    "x": (Math.random() * width).toString() + "px", 
+    "y": (Math.random() * height).toString() + "px"
+  });
+}, 1000);
