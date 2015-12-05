e84ce2f36c60043acc5539099c6e301c2b0df8bd
AlexDWu <hehahohee@gmail.com>
2015-12-04 16:39:24 -0800

    Added a player circle that can be dragged around.

--- client/watchout.js
+++ client/watchout.js
@@ -4,7 +4,6 @@ var width = 600, height = 600;
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
-
 svg.append("image")
   .attr('xlink:href', "asteroid.png")
   .attr("width", 60)
@@ -17,3 +16,19 @@ setInterval(function() {
     "y": (Math.random() * height).toString() + "px"
   });
 }, 1000);
+
+var dragEventListner = d3.behavior.drag();
+svg.append("circle")
+  .attr('class', 'player')
+  .style({
+    "cx": width/2, 
+    "cy":height/2
+  })
+  .call(dragEventListner);
+
+dragEventListner.on("drag", function(dragData){
+  svg.select('.player').style({
+    "cx": d3.event.x,
+    "cy": d3.event.y
+  });
+});
